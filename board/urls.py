d55c7577b003b0a7eb2fcb7b8d026a79e2a0e69e
soohofather <enthchal@gmail.com>
2022-10-07 11:12:13 +0900

    동현 완료

--- board/urls.py
+++ board/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = 'board'
+app_name = "board"
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path("", views.index, name="index"),
+    path("create/", views.create, name="create"),
+    path("<int:pk>/", views.detail, name="detail"),
+    path("<int:pk>/delete/", views.delete, name="delete")
 ]
